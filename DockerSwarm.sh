c062be4bec10f5a7137da4a3d1e7206dfa486895
Neruzzz <ima.rojas.perez@gmail.com>
2023-06-19 18:54:32 +0200

    Web is not visible, now getting ips summing one and assigning. Processes are able to connect and assign IPs.

--- DockerSwarm.sh
+++ DockerSwarm.sh
@@ -24,13 +24,21 @@ echo "Creating new stack"
 docker stack deploy -c open5gs-stack.yml open5gs
 
 echo  "Retrieving all the service IPs"
-service_names=$(docker service ls --format "{{.Name}}")
-echo ${service_names[@]}
 
-service_ips=()
+# Delete old service names and IPs from .env
+sed -i '25,$d' .env
 
-for service_name in "${service_names[@]}"; do
-    service_ips+=$(docker service inspect --format '{{ (index .Endpoint.VirtualIPs 0).Addr }}' $service_name)
+# Run the command and capture the JSON output
+json_output=$(docker network inspect open5gs_default)
+
+# Parse the JSON output and extract service names and IPs
+service_names=($(echo "$json_output" | jq -r '.[0].Containers | keys[]'))
+service_ips=($(echo "$json_output" | jq -r '.[0].Containers[].IPv4Address'))
+
+# Create the .env file and populate it with service names and IPs
+for ((i=0; i<${#service_names[@]}; i++)); do
+    service_name=${service_names[i]%%.*}
+    service_ip=${service_ips[i]%/*}
+    echo "${service_name}=${service_ip}" >> .env
 done
 
-echo ${service_ips[@]}
