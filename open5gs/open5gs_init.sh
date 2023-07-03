84ac7667d996bf64f0d905e4d919bdd55202cc00
Neruzzz <ima.rojas.perez@gmail.com>
2023-07-03 12:21:10 +0200

    Execution

--- open5gs/open5gs_init.sh
+++ open5gs/open5gs_init.sh
@@ -54,11 +54,11 @@ elif [[ "$COMPONENT_NAME" =~ ^(upf-[[:digit:]]+$) ]]; then
 	mkdir -p /mnt/upf/ && \
 	mkdir -p var/log/open5gs && \
 	touch var/log/open5gs/upf.log && \
+	ls var/log/open5gs/ && \
 	chmod 600 var/log/open5gs/upf.log && \
 	cp Open5GS-DockerSwarm/open5gs/config/upf/* /mnt/upf/ && \
 	cp -pfr Open5GS-DockerSwarm/open5gs/config/upf/.env /mnt/upf/ && \
 	/mnt/upf/upf_init.sh && \
-	ifconfig && \
     cd install/bin && ./open5gs-upfd
 elif [[ "$COMPONENT_NAME" =~ ^(webui) ]]; then
 	echo "Deploying component: '$COMPONENT_NAME'"
