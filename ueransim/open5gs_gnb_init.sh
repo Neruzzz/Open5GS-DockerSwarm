5ab99ad57e4f93561a2acee08c7b3b73ab910b23
Neruzzz <ima.rojas.perez@gmail.com>
2023-07-03 12:48:40 +0200

    Execution

--- ueransim/open5gs_gnb_init.sh
+++ ueransim/open5gs_gnb_init.sh
@@ -28,12 +28,14 @@
 
 export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
 
+export $(cat /mnt/ueransim/.env) 2> /dev/null
+
 cp /mnt/ueransim/open5gs-gnb.yaml /UERANSIM/config/open5gs-gnb.yaml
 
 sed -i 's|MNC|'$MNC'|g' /UERANSIM/config/open5gs-gnb.yaml
 sed -i 's|MCC|'$MCC'|g' /UERANSIM/config/open5gs-gnb.yaml
-sed -i 's|NR_GNB_IP|'$NR_GNB_IP'|g' /UERANSIM/config/open5gs-gnb.yaml
-sed -i 's|AMF_IP|'$AMF_IP'|g' /UERANSIM/config/open5gs-gnb.yaml
+sed -i 's|NR_GNB_IP|'$open5gs_nr_gnb'|g' /UERANSIM/config/open5gs-gnb.yaml
+sed -i 's|AMF_IP|'$open5gs_amf'|g' /UERANSIM/config/open5gs-gnb.yaml
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
