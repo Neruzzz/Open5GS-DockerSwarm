79516b1bd5e1651428244419906cc8e6509af257
Neruzzz <ima.rojas.perez@gmail.com>
2023-07-03 12:59:34 +0200

    Execution

--- ueransim/ueransim_image_init.sh
+++ ueransim/ueransim_image_init.sh
@@ -33,12 +33,12 @@ if [[ -z "$COMPONENT_NAME" ]]; then
 	echo "Error: COMPONENT_NAME environment variable not set"; exit 1;
 elif [[ "$COMPONENT_NAME" =~ ^(ueransim-gnb-[[:digit:]]+$) ]]; then
 	echo "Deploying component: '$COMPONENT_NAME'"
-	/mnt/ueransim/open5gs_gnb_init.sh && sleep 10 && \
+	/mnt/ueransim/open5gs_gnb_init.sh && sleep 20 && \
 	./nr-gnb -c ../config/open5gs-gnb.yaml & \
 	bash
 elif [[ "$COMPONENT_NAME" =~ ^(ueransim-ue-[[:digit:]]+$) ]]; then
 	echo "Deploying component: '$COMPONENT_NAME'"
-	/mnt/ueransim/open5gs_ue_init.sh && sleep 10 && \
+	/mnt/ueransim/open5gs_ue_init.sh && sleep 20 && \
 	./nr-ue -c ../config/open5gs-ue.yaml & \
 	bash
 else
